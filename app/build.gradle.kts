721bff18675d1a9093c6893c120b3b13520e553c
Abdoulbasti MUKAILA ALH SHITTU <mukailaabdoulbassit@gmail.com>
2023-12-29 03:46:16 +0100

    Ajout projet, supression question, supression sujet

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     id("com.android.application")
     id("org.jetbrains.kotlin.android")
+    id("com.google.devtools.ksp")
 }
 
 android {
@@ -32,15 +33,19 @@ android {
     compileOptions {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
+        //sourceCompatibility = JavaVersion.VERSION_17
+        //targetCompatibility = JavaVersion.VERSION_17
     }
     kotlinOptions {
         jvmTarget = "1.8"
+        //jvmTarget = "17"
     }
     buildFeatures {
         compose = true
     }
     composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.3"
+        //kotlinCompilerExtensionVersion = "1.4.3"
+        kotlinCompilerExtensionVersion = "1.5.4"
     }
     packaging {
         resources {
@@ -51,6 +56,19 @@ android {
 
 dependencies {
 
+    implementation ("androidx.compose.runtime:runtime-livedata:1.2.1")
+    implementation ("com.google.code.gson:gson:2.10.1")
+    implementation("androidx.room:room-common:2.6.1")
+    implementation("androidx.appcompat:appcompat:1.6.1")
+    implementation("com.google.android.material:material:1.8.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    val room_version = "2.5.2"
+    implementation("androidx.room:room-runtime:$room_version")
+    implementation("androidx.room:room-ktx:$room_version")
+    annotationProcessor("androidx.room:room-compiler:$room_version")
+    ksp("androidx.room:room-compiler:$room_version")
+
+
     implementation("androidx.core:core-ktx:1.9.0")
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
     implementation("androidx.activity:activity-compose:1.7.0")
@@ -59,6 +77,7 @@ dependencies {
     implementation("androidx.compose.ui:ui-graphics")
     implementation("androidx.compose.ui:ui-tooling-preview")
     implementation("androidx.compose.material3:material3")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.6.2")
     implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
     implementation(platform("androidx.compose:compose-bom:2023.03.00"))
--- app/src/main/java/com/example/projetaidememorisation/MainActivityAideMemoire.kt
+++ app/src/main/java/com/example/projetaidememorisation/MainActivityAideMemoire.kt
@@ -1,5 +1,6 @@
 package com.example.projetaidememorisation
 
+import android.annotation.SuppressLint
 import android.app.Activity
 import android.content.Context
 import android.content.Intent
@@ -10,34 +11,170 @@ import androidx.compose.material3.Button
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowDropDown
+import androidx.compose.material3.DropdownMenu
+import androidx.compose.material3.DropdownMenuItem
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.ListItem
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
+import androidx.compose.material3.TextField
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.livedata.observeAsState
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.platform.LocalLifecycleOwner
 import androidx.compose.ui.text.TextStyle
 import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.sp
 import androidx.compose.ui.unit.dp
+import androidx.compose.ui.window.Dialog
+import androidx.lifecycle.viewModelScope
+import androidx.lifecycle.viewmodel.compose.viewModel
 import com.example.projetaidememorisation.aide_memoire.AideMemoire
 import com.example.projetaidememorisation.creation_suppression.CreationSuppression
+import com.example.projetaidememorisation.creation_suppression.sujets_importation.PJson
+import com.example.projetaidememorisation.creation_suppression.sujets_importation.Sujet
+import com.example.projetaidememorisation.data.SujetsQuestions
 import com.example.projetaidememorisation.parametrage.Parametrage
+//import androidx.compose.runtime.flow.collectAsState
+import com.google.gson.Gson
+import kotlinx.coroutines.launch
 
 class MainActivityAideMemoire : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
-            MonEcran()
+
+            //InterfaceSuppressionSujetsOuQuestions()
+            //AjouterNouveauSujetComposable()
+
+            //MonEcran()
         }
     }
 }
 
+//OK
+@Composable
+fun InterfaceSuppressionSujetsOuQuestions() {
+    val context = LocalContext.current
+    val viewModel: ViewModelBDD = viewModel(
+        factory = ViewModelBDDFactory(context.applicationContext as JeuQuestionnaireApplication)
+    )
+
+    val sujets by viewModel.sujets.observeAsState(listOf())
+    val questionsParSujet by viewModel.questionsParSujet.observeAsState(mapOf())
+    val expandedState = rememberSaveable { mutableStateOf(mutableMapOf<Int, Boolean>()) }
+
+    LazyColumn {
+        items(sujets) { sujet ->
+            val isExpanded = expandedState.value[sujet.sujetId] ?: false
+
+            Column {
+                Row(
+                    modifier = Modifier.fillMaxWidth(),
+                    verticalAlignment = Alignment.CenterVertically
+                ) {
+                    Text(
+                        text = sujet.nomSujet,
+                        modifier = Modifier.weight(1f)
+                    )
+                    IconButton(
+                        onClick = {
+                            expandedState.value = expandedState.value.toMutableMap().apply {
+                                this[sujet.sujetId] = !isExpanded
+                            }
+                        }
+                    ) {
+                        Icon(Icons.Default.ArrowDropDown, contentDescription = "Déplier/Réduire")
+                    }
+                    Spacer(Modifier.width(8.dp))
+                    Button(onClick = { viewModel.supprimerSujetParNom(sujet.nomSujet) }) {
+                        Text("Supprimer Sujet")
+                    }
+                }
+                if (isExpanded) {
+                    questionsParSujet[sujet.sujetId]?.forEach { question ->
+                        Row(
+                            modifier = Modifier.fillMaxWidth().padding(start = 16.dp),
+                            verticalAlignment = Alignment.CenterVertically
+                        ) {
+                            Text(
+                                text = question.texteQuestion,
+                                modifier = Modifier.weight(1f)
+                            )
+                            Spacer(Modifier.width(8.dp))
+                            Button(onClick = { viewModel.supprimerQuestionParTexteEtSujet(question.texteQuestion, sujet.nomSujet) }) {
+                                Text("Supprimer Question")
+                            }
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
+
+
+
+//Ok
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun AjouterNouveauSujetComposable() {
+    // Contexte local et ViewModel
+    val context = LocalContext.current
+    val viewModel: ViewModelBDD = viewModel(
+        factory = ViewModelBDDFactory(context.applicationContext as JeuQuestionnaireApplication)
+    )
+    var nomSujet by rememberSaveable { mutableStateOf("") }
+
+    Column(modifier = Modifier.padding(16.dp)) {
+        // Champ de texte pour le nom du sujet
+        OutlinedTextField(
+            value = nomSujet,
+            onValueChange = { nomSujet = it },
+            label = { Text("Nom du Sujet") },
+            singleLine = true,
+            modifier = Modifier.fillMaxWidth()
+        )
+
+        Spacer(Modifier.height(16.dp))
+
+        // Bouton pour ajouter un sujet
+        Button(
+            onClick = {
+                if (nomSujet.isNotBlank()) {
+                    viewModel.ajouterNouveauSujet(nomSujet)
+                    nomSujet = "" // Réinitialiser le champ après l'ajout
+                }
+            },
+            modifier = Modifier.fillMaxWidth(),
+            enabled = nomSujet.isNotBlank()
+        ) {
+            Text("Ajouter Sujet")
+        }
+    }
+}
 
 @Composable
 fun titreActivite(titre : String, couleur : Color) {
@@ -50,36 +187,16 @@ fun titreActivite(titre : String, couleur : Color) {
         verticalAlignment = Alignment.CenterVertically
 
     ) {
-        Text(text = titre, style = TextStyle(
+        Text(text = titre,
+            style = TextStyle(
             fontSize = 20.sp, // Taille de police de 20sp (changement ici)
             fontWeight = FontWeight.Bold,
             color = Color.White
-        )
+            )
         )
     }
 }
 
-/*@Composable
-fun QuitAppButton(c: Context,saisie: String) {
-    val activity = LocalContext.current
-    Row(
-        modifier = Modifier.fillMaxWidth(),
-        horizontalArrangement = Arrangement.Center
-    ) {
-        Spacer(modifier = Modifier.width(5.dp))
-        Button(onClick = {
-            val a= c as Activity
-            val intent = Intent()
-            //val intent = Intent(this, MainActivityAideMemoire::class.java)
-            intent.flags = Intent.FLAG_ACTIVITY_NEW_TASK or Intent.FLAG_ACTIVITY_CLEAR_TASK
-            //intent.putExtra("nom", saisie) //ajouter des données à l'Intent
-            a.setResult(Activity.RESULT_OK, intent) // enregistrer le résultat de l'activité
-            a.finish() //terminer l'activité fille
-        }) {
-            Text("Quitter l'application")
-        }
-    }
-}*/
 
 @Composable
 fun buttonVersActivite(activityClass: Class<*>, text : String, context : Context) {
@@ -97,10 +214,11 @@ fun buttonVersActivite(activityClass: Class<*>, text : String, context : Context
 
 
 
+
+//@SuppressLint("CoroutineCreationDuringComposition")
 @Composable
 fun MonEcran() {
     val context= LocalContext.current
-
 
     Column (
         //modifier = Modifier.fillMaxWidth(), // Cela étire la colonne pour occuper toute la largeur disponible
